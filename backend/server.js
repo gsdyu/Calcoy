ac4ffda7389b095ca35c2f7c32511cbd5c445f3e
MinaHanna34 <115050095+MinaHanna34@users.noreply.github.com>
2024-08-28 21:15:25 -0700

    test
    test

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow  
+    origin: 'http://localhost:3000',  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  
     credentials: true  
 }));
