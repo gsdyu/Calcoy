c2bbddce4df0e3d3801bba2ccdbd5aafc1c2277c
gsdyu <justinchong203@gmail.com>
2024-09-04 19:16:55 -0700

    added base groq javascript

--- backend/ai/test.js
+++ backend/ai/test.js
@@ -1,14 +1,17 @@
-const OpenAI = require("openai");
-require('dotenv').config({path:"../.env"});
-openai = new OpenAI()
+const Groq = require("groq-sdk")
+
+const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
 
 async function main() {
-	const completion = await openai.chat.completions.create({
-		model: "gpt-4o-mini",
-		messages: [{role: "user", content: "hello"}]});
-	console.log(completion.choices[0].message);
+	const completion = groq.chat.completions.create({
+		messages:[
+			{
+				role: "user",
+				content: "Explain the importance of fast language models",
+			}
+		],
+		model: "llama3-8b-8192",
+	});
+	
+	console.log(completion.choices[0]?.message?.content || "");
 }
-
-//main();
-var events = require("./test.json");
-console.log(events);
