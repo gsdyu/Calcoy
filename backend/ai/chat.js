7a0303c73f44008a1b44405520ee044d94f8ca06
gsdyu <justinchong203@gmail.com>
2024-09-04 21:51:38 -0700

    basic implemention of chatbot with groq (not frontend implemented)
    openai api is not free anymore i do not think. used groq api cause free
    api. you need to sign up for api key from groq.
    
    groq is prone to prompt injection; not secure. as of now you can ask it
    to ignore the system message and it will :(
    
    the syntax between groq and the openai are very similar; groq can be use
    as a free temp until we want to actually specifically openai models.

--- backend/ai/chat.js
+++ backend/ai/chat.js
@@ -3,50 +3,53 @@ require ('dotenv').config({path:'../.env'})
 
 //const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
 
+const client = new Groq({});
+
+//parameters for groq
 var model = "llama3-8b-8192";
 var history = [];
-async function initChat(system = {role: "system", content: "You will talk like Hal 9000"}) {
-	const groq = new Groq({});
-	var model = "llama3-8b-8192"
+var max_tokens = 100;
+var temperature = 1;
+var stream = false;
+
+async function initChat(system = {role: "system", content: "You are an assistant and also Frankenstein's monster."}) {
 	history.push(system);
-	var check = groq.chat.completions.create({
-			messages: history,
-			model: model,
-		}
-	)
+	var check = client.chat.completions.create({
+		messages: history,
+		model: model,
+		max_tokens: 100,
+		})
 	return 1;
 }
 
 async function inputChat(input) {
-
+	history.push({role: "user", content: input});
+	var completions = await client.chat.completions.create({
+		messages: history,
+		model: model,
+		temperature: temperature,
+		max_tokens: max_tokens,
+	});
+	var message = completions.choices[0].message.content; 
+	history.push(message);
+	return message;
 }
 
 function clearChat(priorSystem){
 	if (typeof(priorSystem) != typeof(true)){
-		throw new TypeError("Input should be actual boolean");
+		throw new TypeError("Input should be an actual boolean");
 	}
-	if (priorSystem) {
+	if (priorSystem && history.length != 0) {
 		history = [history[0]];	
 	}
 	history = [];
 	return 1;
 }
 
-initChat().then(value=>console.log(value)).catch(reason => {console.log("Caught failure "+ reason); return "nothing";});
+// conversation with frakenstein assistant
+initChat();
+inputChat("Is your name Frankenstein or Frankeinstein's monster?").then(value=>console.log(value)).catch(reason=>console.log(reason));
 
-async function main() {
-	const completion = await groq.chat.completions.create({
-		messages:[
-			{
-				role: "user",
-				content: "Explain the importance of fast language models",
-			}
-		],
-		model: "llama3-8b-8192",
-	});
-	
-	console.log(completion.choices[0]?.message?.content || "error");
-}
 
 
 
