1aa13eb54280fb3a46075632d8f01ad8ad059f61
ttoanttran <126648228+ttoanttran@users.noreply.github.com>
2024-09-25 15:27:19 -0700

    Add Ai page css

--- frontend/src/components/AI/AiPage.js
+++ frontend/src/components/AI/AiPage.js
@@ -3,34 +3,41 @@
 import styles from './AiPage.module.css';
 import { useState } from 'react';
 
-const AiPage = () => {
-    
-    function TextInput() {
-        const [inputValue, setInputValue] = useState('');
-      
-        const handleInputChange = (event) => {
-          setInputValue(event.target.value);
-        };
-      
-        return (
-          <div>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              placeholder="How can I help?"
-              className={styles.inputText}
-            />
-          </div>
-        );
-      }
+function TextInput() {
+  const [inputValue, setInputValue] = useState('');
 
-    return (
-        <>
-            <h1>Timewise AI</h1>
-            <TextInput></TextInput>
-        </>
-    );
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+    event.target.style.height = 'auto';
+    event.target.style.height = `${event.target.scrollHeight}px`;
+  };
+
+  return (
+    <div className={styles.inputContainer}>
+      <textarea
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Ask Timewise AI"
+        className={styles.inputText}
+        rows={1}
+      />
+    </div>
+  );
+}
+
+const AiPage = () => {
+  
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.headerContainer}>
+        <h1 className={styles.header}>Timewise AI</h1>
+        <h2 className={styles.subheader}>How can I help you?</h2>
+      </div>
+
+      <TextInput />
+    </div>
+  );
 }
 
 export default AiPage;
