3c6229edbebd92461ac0ace758892c4512558551
DeathGumi <dueldnh@gmail.com>
2024-10-09 09:44:04 -0700

    config the server again...

--- backend/index.js
+++ backend/index.js
@@ -115,5 +115,5 @@ app.get('/', async (req, res) => {
 })
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
