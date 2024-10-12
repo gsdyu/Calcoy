db00a69a300a13b20bd968e9f55be957a0a9ecbd
ttoanttran <126648228+ttoanttran@users.noreply.github.com>
2024-10-11 19:32:46 -0700

    Add Event small ui changes

--- frontend/src/components/Modals/AddEditEventModal.js
+++ frontend/src/components/Modals/AddEditEventModal.js
@@ -1,8 +1,9 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { X } from 'lucide-react';
+import { Divide, X } from 'lucide-react';
 import { useTheme } from '@/contexts/ThemeContext';
+
 
 const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
   const { darkMode } = useTheme();
@@ -56,39 +57,43 @@ const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className={`${darkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'} p-6 rounded-lg w-96`}>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={onClose} >
+      <div className={`${darkMode ? 'bg-gray-800 text-gray-200' : 'bg-white text-black'} p-7 rounded-xl w-[550px]`} onClick={(e) => e.stopPropagation()}>
         <div className="flex justify-between items-center mb-4">
-          <h3 className="text-xl font-bold">Add/Edit Event</h3>
+          <h3 className="text-2xl font-medium">Create/Edit Event</h3>
           <button onClick={onClose} className="text-gray-400 hover:text-gray-200">
-            <X size={20} />
+            <X size={25} />
           </button>
         </div>
+        <div className='bg'></div>
         <form onSubmit={handleSubmit}>
+        <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Event name</label>
           <input
             type="text"
             name="title"
-            placeholder="Event Title"
+            placeholder="Enter event name"
             value={newEvent.title}
             onChange={handleChange}
-            className={`w-full p-2 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
             required
           />
+          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Date</label>
           <input
             type="date"
             name="date"
             value={newEvent.date}
             onChange={handleChange}
-            className={`w-full p-2 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
             required
           />
+          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Time</label>
           <div className="flex justify-between mb-4">
             <input
               type="time"
               name="startTime"
               value={newEvent.startTime}
               onChange={handleChange}
-              className={`w-5/12 p-2 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+              className={`w-5/12 p-3 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
               required
             />
             <input
@@ -96,23 +101,25 @@ const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
               name="endTime"
               value={newEvent.endTime}
               onChange={handleChange}
-              className={`w-5/12 p-2 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+              className={`w-5/12 p-3 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
               required
             />
           </div>
+          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Location</label>
           <input
             type="text"
             name="location"
-            placeholder="Location"
+            placeholder="Add location"
             value={newEvent.location}
             onChange={handleChange}
-            className={`w-full p-2 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
           />
+          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Frequency</label>
           <select
             name="frequency"
             value={newEvent.frequency}
             onChange={handleChange}
-            className={`w-full p-2 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
           >
             <option>Does not repeat</option>
             <option>Daily</option>
@@ -120,21 +127,22 @@ const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
             <option>Monthly</option>
             <option>Yearly</option>
           </select>
+          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Occasion</label>
           <select
             name="calendar"
             value={newEvent.calendar}
             onChange={handleChange}
-            className={`w-full p-2 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-300'} border rounded`}
+            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
           >
             <option>Personal</option>
             <option>Work</option>
             <option>Family</option>
           </select>
-          <div className="flex justify-between">
-            <button type="button" onClick={onClose} className={`px-4 py-2 ${darkMode ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-300 hover:bg-gray-400'} rounded`}>
+          <div className="flex justify-end space-x-4 pt-5">
+            <button type="button" onClick={onClose} className={`px-4 py-2 ${darkMode ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-300 hover:bg-gray-400'} rounded-[7px]`}>
               Cancel
             </button>
-            <button type="submit" className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded">
+            <button type="submit" className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-[7px]">
               Save
             </button>
           </div>
