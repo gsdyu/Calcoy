1056b9cb1776cdf1d50fd708c562ad1ae7d6021b
ttoanttran <126648228+ttoanttran@users.noreply.github.com>
2024-10-12 23:18:15 -0700

    add conditional rendering for event/task

--- frontend/src/components/Modals/AddEditEventModal.js
+++ frontend/src/components/Modals/AddEditEventModal.js
@@ -19,6 +19,13 @@ const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
     calendar: 'Personal'
   });
 
+  const [newTask, setNewTask] = useState({
+    title: '',
+    date: initialDate ? initialDate.toISOString().split('T')[0] : new Date().toISOString().split('T')[0],
+    time: '',
+    frequency: 'Does not repeat',
+  });
+
   useEffect(() => {
     setIsVisible(true);
 
@@ -34,35 +41,55 @@ const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
         date: initialDate.toISOString().split('T')[0],
         startTime: initialDate.toTimeString().slice(0, 5)
       }));
+      setNewTask(prev => ({
+        ...prev,
+        date: initialDate.toISOString().split('T')[0],
+        time: initialDate.toTimeString().slice(0, 5)
+      }));
     }
   }, [initialDate]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setNewEvent(prev => ({ ...prev, [name]: value }));
+    if (selected === 'event') {
+      setNewEvent(prev => ({ ...prev, [name]: value }));
+    } else {
+      setNewTask(prev => ({ ...prev, [name]: value }));
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const startDateTime = new Date(`${newEvent.date}T${newEvent.startTime}`);
-    const endDateTime = new Date(`${newEvent.date}T${newEvent.endTime}`);
-  
-    if (endDateTime <= startDateTime) {
-      alert('End time must be after start time');
-      return;
+    if (selected === 'event') {
+      const startDateTime = new Date(`${newEvent.date}T${newEvent.startTime}`);
+      const endDateTime = new Date(`${newEvent.date}T${newEvent.endTime}`);
+
+      if (endDateTime <= startDateTime) {
+        alert('End time must be after start time');
+        return;
+      }
+
+      const event = {
+        title: newEvent.title,
+        location: newEvent.location,
+        start_time: startDateTime.toISOString(),
+        end_time: endDateTime.toISOString(),
+        time_zone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        frequency: newEvent.frequency,
+        calendar: newEvent.calendar
+      };
+      console.log('Event being saved:', event);
+      onSave(event);
+    } else {
+      const task = {
+        title: newTask.title,
+        date: newTask.date,
+        time: newTask.time,
+        frequency: newEvent.frequency,
+      };
+      console.log('Task being saved:', task);
+      onSave(task);
     }
-  
-    const event = {
-      title: newEvent.title,
-      location: newEvent.location,
-      start_time: startDateTime.toISOString(),
-      end_time: endDateTime.toISOString(),
-      time_zone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-      frequency: newEvent.frequency,
-      calendar: newEvent.calendar
-    };
-    console.log('Event being saved:', event);
-    onSave(event);
     handleClose();
   };
 
@@ -115,77 +142,125 @@ const AddEditEventModal = ({ onClose, onSave, initialDate }) => {
           </button>
         </div>
         <form onSubmit={handleSubmit}>
-        <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Event name</label>
-          <input
-            type="text"
-            name="title"
-            placeholder="Enter event name"
-            value={newEvent.title}
-            onChange={handleChange}
-            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-            required
-          />
-          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Date</label>
-          <input
-            type="date"
-            name="date"
-            value={newEvent.date}
-            onChange={handleChange}
-            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-            required
-          />
-          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Time</label>
-          <div className="flex justify-between mb-4">
-            <input
-              type="time"
-              name="startTime"
-              value={newEvent.startTime}
-              onChange={handleChange}
-              className={`w-5/12 p-3 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-              required
-            />
-            <input
-              type="time"
-              name="endTime"
-              value={newEvent.endTime}
-              onChange={handleChange}
-              className={`w-5/12 p-3 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-              required
-            />
-          </div>
-          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Location</label>
-          <input
-            type="text"
-            name="location"
-            placeholder="Add location"
-            value={newEvent.location}
-            onChange={handleChange}
-            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-          />
-          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Frequency</label>
-          <select
-            name="frequency"
-            value={newEvent.frequency}
-            onChange={handleChange}
-            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-          >
-            <option>Does not repeat</option>
-            <option>Daily</option>
-            <option>Weekly</option>
-            <option>Monthly</option>
-            <option>Yearly</option>
-          </select>
-          <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Occasion</label>
-          <select
-            name="calendar"
-            value={newEvent.calendar}
-            onChange={handleChange}
-            className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
-          >
-            <option>Personal</option>
-            <option>Work</option>
-            <option>Family</option>
-          </select>
+          {selected === 'event' ? (
+            <>
+              <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Event name</label>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Enter event name"
+                  value={newEvent.title}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                  required
+                />
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Date</label>
+                <input
+                  type="date"
+                  name="date"
+                  value={newEvent.date}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                  required
+                />
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Time</label>
+                <div className="flex justify-between mb-4">
+                  <input
+                    type="time"
+                    name="startTime"
+                    value={newEvent.startTime}
+                    onChange={handleChange}
+                    className={`w-5/12 p-3 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                    required
+                  />
+                  <input
+                    type="time"
+                    name="endTime"
+                    value={newEvent.endTime}
+                    onChange={handleChange}
+                    className={`w-5/12 p-3 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                    required
+                  />
+                </div>
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Location</label>
+                <input
+                  type="text"
+                  name="location"
+                  placeholder="Add location"
+                  value={newEvent.location}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                />
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Frequency</label>
+                <select
+                  name="frequency"
+                  value={newEvent.frequency}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                >
+                  <option>Does not repeat</option>
+                  <option>Daily</option>
+                  <option>Weekly</option>
+                  <option>Monthly</option>
+                  <option>Yearly</option>
+                </select>
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Occasion</label>
+                <select
+                  name="calendar"
+                  value={newEvent.calendar}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                >
+                  <option>Personal</option>
+                  <option>Work</option>
+                  <option>Family</option>
+                </select>
+              </>
+            ) : (
+              <>
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Task title</label>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Enter task title"
+                  value={newTask.title}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                  required
+                />
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Date</label>
+                <input
+                  type="date"
+                  name="date"
+                  value={newTask.date}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                  required
+                />
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Time</label>
+                <input
+                  type="time"
+                  name="time"
+                  value={newTask.time}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                  required
+                />
+                <label className={`${darkMode ? ' text-gray-400' : 'text-black'} block font-medium pb-1`}>Frequency</label>
+                <select
+                  name="frequency"
+                  value={newTask.frequency}
+                  onChange={handleChange}
+                  className={`w-full p-3 mb-4 ${darkMode ? 'bg-gray-700 border-gray-600' : 'bg-gray-100 border-gray-100'} rounded-[7px]`}
+                >
+                  <option>Does not repeat</option>
+                  <option>Daily</option>
+                  <option>Weekly</option>
+                  <option>Monthly</option>
+                  <option>Yearly</option>
+                </select>
+              </>
+            )}
           <div className="flex justify-end space-x-4 pt-5">
             <button type="button" onClick={onClose} className={`px-4 py-2 ${darkMode ? 'bg-gray-700 hover:bg-gray-600' : 'bg-gray-300 hover:bg-gray-400'} rounded-[7px]`}>
               Cancel
