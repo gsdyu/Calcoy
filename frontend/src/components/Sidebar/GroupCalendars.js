bf7d386c619c3ae4236db5ed8a2086c4cabe315a
gsdyu <justinchong203@gmail.com>
2024-10-31 22:45:43 -0700

    updated script to drop tables; users

--- frontend/src/components/Sidebar/GroupCalendars.js
+++ frontend/src/components/Sidebar/GroupCalendars.js
@@ -106,9 +106,9 @@ const GroupCalendars = ({ toggleSidebar, isSidebarOpen, activeCalendar, setActiv
               {/* Right Border Effect */}
               <div
                 className={`absolute right-0 top-1/2 transform -translate-y-1/2 transition-all duration-200 origin-center ${
-                  activeCalendar.id === server.id
+                  activeCalendar === server.id
                     ? 'bg-white w-1 h-full scale-y-100' 
-                    : hoveredServer.id === server.id
+                    : hoveredServer === server.id
                     ? 'bg-white w-1 h-4 scale-y-125' 
                     : 'w-0 h-1'  
                 }`}
