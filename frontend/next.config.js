915ff1cb3b42e15ec5c13aa2cc75a12355c6be58
MinaHanna34 <115050095+MinaHanna34@users.noreply.github.com>
2024-11-10 23:36:31 -0800

    fixes export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
+  output: 'export',      // Use 'export' for static output
+  distDir: 'out',        // Specify the output directory as 'out'
   webpack: (config) => {
     config.plugins.push(new CaseSensitivePathsPlugin());
     return config;
