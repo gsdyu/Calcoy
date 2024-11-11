49b0cc7f2c7e768e88dce8d1722e14e62b1d4407
MinaHanna34 <115050095+MinaHanna34@users.noreply.github.com>
2024-11-10 19:59:07 -0800

    fix-work

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 // frontend/next.config.js
 /** @type {import('next').NextConfig} */
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
 const nextConfig = {
   output: 'export',
+  webpack: (config) => {
+    config.plugins.push(new CaseSensitivePathsPlugin());
+    return config;
+  },
 };
 
 module.exports = nextConfig;
