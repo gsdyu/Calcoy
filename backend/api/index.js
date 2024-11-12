d95e2a697a6835079a6705277bd763235b48a728
gsdyu <justinchong203@gmail.com>
2024-11-11 18:32:51 -0800

    remove useless file for production

--- backend/api/index.js
+++ backend/api/index.js
@@ -117,6 +117,6 @@ app.get('/', async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running`));
 
 module.exports = app;
