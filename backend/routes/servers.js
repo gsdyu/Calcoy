edc5619483955487ec86964ba21111401582b8b9
MinaHanna34 <115050095+MinaHanna34@users.noreply.github.com>
2024-11-13 22:00:21 -0800

    fixed server issue

--- backend/routes/servers.js
+++ backend/routes/servers.js
@@ -6,16 +6,14 @@ const { authenticateToken } = require('../authMiddleware');
 const upload = multer({ dest: 'uploads/' });
 const { v4: uuidv4 } = require('uuid');
 module.exports = (app, pool) => {
-
   app.get('/api/user', authenticateToken, async (req, res) => {
-    const userId = req.user.userId; // Retrieve userId directly from the token
+    const userId = req.user.userId;  
     if (userId) {
       res.json({ userId });
     } else {
       res.status(401).json({ error: 'User not authenticated' });
     }
   });
-
   app.get('/api/servers/:serverId', authenticateToken, async (req, res) => {
     const { serverId } = req.params;
   
@@ -41,9 +39,9 @@ module.exports = (app, pool) => {
   
     try {
       const { rows } = await pool.query(
-       `SELECT users.username, users.email FROM "userServers" 
-        INNER JOIN users ON "userServers".user_id = users.id
-        WHERE "userServers".server_id = $1`, [serverId]
+       `SELECT users.username, users.email FROM user_servers 
+        INNER JOIN users ON user_servers.user_id = users.id
+        WHERE user_servers.server_id = $1`, [serverId]
       );
       if (rows.rowCount === 0) {
         return res.status(404).json({error: "Server not found or there are no users."});
@@ -57,94 +55,26 @@ module.exports = (app, pool) => {
   })
   
     // Route to leave a server
-    app.delete('/api/servers/:serverId/leave', authenticateToken, async (req, res) => {
-      const userId = req.user.userId;
-      const { serverId } = req.params;
-  
-      try {
-        // Delete the association between the user and the server
-        const result = await pool.query(
-          `DELETE FROM "userServers" WHERE user_id = $1 AND server_id = $2 RETURNING *`,
-          [userId, serverId]
-        );
-  
-        if (result.rowCount === 0) {
-          return res.status(404).json({ error: 'Server not found or user not a member' });
-        }
-  
-        res.json({ message: 'Successfully left the server' });
-      } catch (err) {
-        console.error('Error leaving server:', err);
-        res.status(500).json({ error: 'Error leaving server' });
-      }
-    });
- 
-    app.post('/api/servers/join', authenticateToken, upload.none(), async (req, res) => {
-      const { inviteLink } = req.body;
-      const userId = req.user.userId;
-    
-      try {
-        if (!inviteLink || typeof inviteLink !== 'string') {
-          return res.status(400).json({ error: 'Invalid invite link format' });
-        }
-    
-        const inviteIdentifier = inviteLink.split('/').pop();
-    
-        const serverResult = await pool.query('SELECT id FROM servers WHERE invite_link = $1', [inviteIdentifier]);
-    
-        if (serverResult.rowCount === 0) {
-          return res.status(404).json({ error: 'Invalid invite link' });
-        }
-    
-        const serverId = serverResult.rows[0].id;
-    
-        const userServerCheck = await pool.query(
-          'SELECT 1 FROM "userServers" WHERE user_id = $1 AND server_id = $2',
-          [userId, serverId]
-        );
-    
-        if (userServerCheck.rowCount > 0) {
-          return res.status(200).json({ message: 'Already joined' });
-        }
-    
-        await pool.query('INSERT INTO "userServers" (user_id, server_id) VALUES ($1, $2)', [userId, serverId]);
-    
-        res.status(201).json({ message: 'Successfully joined the server', serverId });
-      } catch (error) {
-        console.error('Error joining server:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    });
-    
+  app.delete('/api/servers/:serverId/leave', authenticateToken, async (req, res) => {
+    const userId = req.user.userId;
+    const { serverId } = req.params;
 
-    app.post('/api/servers/create', authenticateToken, upload.single('icon'), async (req, res) => {
-      const { serverName } = req.body;
-      const userId = req.user.userId;
-      const icon = req.file;
-    
-      try {
-        const iconPath = icon ? `/uploads/${icon.filename}` : null;
-        const inviteLink = uuidv4(); // Generate a unique invite link
-    
-        const { rows } = await pool.query(
-          `INSERT INTO servers (name, image_url, created_by, invite_link) VALUES ($1, $2, $3, $4) RETURNING *`,
-          [serverName, iconPath, userId, inviteLink]
-        );
-    
-        const server = rows[0];
-    
-        await pool.query(
-          `INSERT INTO "userServers" (user_id, server_id) VALUES ($1, $2)`,
-          [userId, server.id]
-        );
-    
-        return res.json({ server });
-      } catch (err) {
-        console.error('Error creating server:', err);
-        return res.status(500).json({ error: 'Error creating server' });
+    try {
+      // Delete the association between the user and the server
+      const result = await pool.query(
+        `DELETE FROM user_servers WHERE user_id = $1 AND server_id = $2 RETURNING *`,
+        [userId, serverId]
+      );
+
+      if (result.rowCount === 0) {
+        return res.status(404).json({ error: 'Server not found or user not a member' });
       }
 
-      return res.json({ message: 'Successfully left the server' });
+      res.json({ message: 'Successfully left the server' });
+    } catch (err) {
+      console.error('Error leaving server:', err);
+      res.status(500).json({ error: 'Error leaving server' });
+    }
   });
  
   app.post('/api/servers/join', authenticateToken, upload.none(), async (req, res) => {
@@ -167,7 +97,7 @@ module.exports = (app, pool) => {
       const serverId = serverResult.rows[0].id;
   
       const userServerCheck = await pool.query(
-        'SELECT 1 FROM "userServers" WHERE user_id = $1 AND server_id = $2',
+        'SELECT 1 FROM user_servers WHERE user_id = $1 AND server_id = $2',
         [userId, serverId]
       );
   
@@ -175,7 +105,7 @@ module.exports = (app, pool) => {
         return res.status(200).json({ message: 'Already joined' });
       }
   
-      await pool.query('INSERT INTO "userServers" (user_id, server_id) VALUES ($1, $2)', [userId, serverId]);
+      await pool.query('INSERT INTO user_servers (user_id, server_id) VALUES ($1, $2)', [userId, serverId]);
   
       res.status(201).json({ message: 'Successfully joined the server', serverId });
     } catch (error) {
@@ -202,7 +132,7 @@ module.exports = (app, pool) => {
       const server = rows[0];
   
       await pool.query(
-        `INSERT INTO "userServers" (user_id, server_id) VALUES ($1, $2)`,
+        `INSERT INTO user_servers (user_id, server_id) VALUES ($1, $2)`,
         [userId, server.id]
       );
   
@@ -220,8 +150,8 @@ module.exports = (app, pool) => {
     try {
       const { rows } = await pool.query(
         `SELECT servers.* FROM servers
-         JOIN "userServers" ON servers.id = "userServers".server_id
-         WHERE "userServers".user_id = $1`, 
+         JOIN user_servers ON servers.id = user_servers.server_id
+         WHERE user_servers.user_id = $1`, 
         [userId]
       );
 
